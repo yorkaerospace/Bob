f85aa4515aa5674b1f934394667ad25f50ff3ebe
ProtonNumber <jwhinton@yenno.com>
2022-07-11 18:55:44 +0100

    Missed a semicolon

--- firmware/src/utils.h
+++ firmware/src/utils.h
@@ -5,6 +5,6 @@
 
 /* Reads a number of bytes from an i2c device, starting at the register start
    Puts the bytes at dest. */
-void i2cReadBytes(i2c_t *i2c, uint8_t addr, uint8_t start, uint8_t bytes, void *dest)
+void i2cReadBytes(i2c_t *i2c, uint8_t addr, uint8_t start, uint8_t bytes, void *dest);
 
 #endif
