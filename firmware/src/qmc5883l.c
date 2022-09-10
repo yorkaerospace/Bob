ee2a04005e5391f01d3cefced1a7dda35f7e8204
ProtonNumber <jwhinton@yenno.com>
2022-09-10 20:48:14 +0100

    Started work on a magnetometer driver

---
+++ firmware/src/qmc5883l.c
@@ -0,0 +1,139 @@
+#include "qmc5883l.h"
+
+/* Wrapper around i2c write function */
+static int QMCWriteByte(qmc_t * sensor, enum QMCRegister reg, uint8_t value) {
+    uint8_t buffer[2];
+    buffer[0] = reg;
+    buffer[1] = value;
+    return i2c_write_timeout_us(sensor->i2c, QMC_ADDR, &buffer,
+                                2, true, QMC_TIMEOUT);
+}
+
+/* Wrapper around i2c read function */
+static int QMCReadBytes(qmc_t * sensor, enum QMCRegister reg, size_t len, uint8_t * buffer) {
+    uint8_t regShort = reg; // Trim the enum to a uint8_t.
+    i2c_write_timeout_us(sensor->i2c, QMC_ADDR, &regShort,
+                        1, true, QMC_TIMEOUT);
+    return i2c_read_timeout_us(sensor->i2c, QMC_ADDR, buffer,
+                               len, false, QMC_TIMEOUT);
+}
+
+/* Creates a QMC_T */
+qmc_t QMCInit(i2c_inst_t * i2c) {
+    qmc_t sensor;
+    sensor.i2c = i2c;
+    // Attempt to get the current config
+    QMCGetCfg(&sensor);
+    return sensor;
+}
+
+/* The QMC5883L doesnt have any real self-test capability, but we can
+ * at least make sure it is talking properly and if it is configured correctly.
+ * Returns:
+ * 0 if the QMC is talking and is configured to produce data.
+ * 1 if the QMC is talking, has a valid configuration, but is on standby.
+ * 2 if the QMC has an invalid configuration.
+ * 3 if the QMC is not talking */
+uint8_t QMCTest(qmc_t * sensor) {
+    switch(QMCGetCfg(sensor)) {
+    case 0:
+        if(sensor->config.mode != QMC_STANDBY) {
+            return 0;
+        } else {
+            return 1;
+        }
+    case 1:
+        return 2;
+    case 2:
+        return 3;
+    default:        // Should never happen, but makes the compiler happy.
+        return 3;
+    }
+}
+
+/* Reads and parses the QMC status register, places the result in status
+ * Returns:
+ * 0 if successful
+ * 1 if the I2C read failed */
+uint8_t QMCGetStatus(qmc_t * sensor, struct qmc_status * status) {
+    uint8_t reg;
+    if(QMCReadBytes(sensor, QMC_STATUS, 1, &reg) == 1) {
+        status->dataReady = reg & 1 << QMC_DRDY;
+        status->dataOverflow = reg & 1 << QMC_DOVL;
+        status->dataSkip = reg & 1 << QMC_DSKIP;
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+/* A quick way to configure the QMC5883L, based on config.
+ * N.B. the control 1 & 2 raw fields in config can be safely ignored
+ * Returns:
+ * 0 if successful
+ * 1 if the I2C failed */
+uint8_t QMCSetCfg(qmc_t * sensor, struct qmc_cfg config) {
+
+    config.control1Raw = config.mode            << QMC_MODE_SHIFT
+                       | config.ODR             << QMC_ODR_SHIFT
+                       | config.OSR             << QMC_OSR_SHIFT
+                       | config.scale           << QMC_SCALE_SHIFT;
+
+    config.control2Raw = config.pointerRoll     << QMC_ROL_PNT
+                       | config.enableInterrupt << QMC_INT_ENB;
+
+    /* The QMC only increments some pointers, so configuration has to be done as
+     * 2 seperate writes. */
+    if(QMCWriteByte(sensor, QMC_CONTROL1, config.control1Raw) == 2 &&
+       QMCWriteByte(sensor, QMC_CONTROL2, config.control2Raw) == 2) {
+        sensor->config = config;
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+/* Reads and parses the config registers on the QMC5883L
+ * Stores the result in sensor->config, alongside the raw registers.
+ * Returns:
+ * 0 if successful
+ * 1 if the config is invalid
+ * 2 if the I2C failed */
+uint8_t QMCGetCfg(qmc_t * sensor) {
+    uint8_t buffer[2];
+    bool invalid = false;
+
+    // Again, the pointers dont increment, so we need to do 2 reads.
+    if(QMCReadBytes(sensor, QMC_CONTROL1, 1, buffer) == 1 &&
+       QMCReadBytes(sensor, QMC_CONTROL2, 1, buffer + 1) == 1) {
+
+        sensor->config.ODR = (buffer[0] >> QMC_ODR_SHIFT) & 3;
+        sensor->config.OSR = (buffer[0] >> QMC_OSR_SHIFT) & 3;
+        sensor->config.mode = buffer[0] & 1;
+        sensor->config.scale = (buffer[0] >> QMC_SCALE_SHIFT) & 1;
+
+        if(buffer[0] & 3 > 1 || (buffer[0] >> QMC_SCALE_SHIFT) & 3 > 1) {
+            invalid = true;
+        }
+
+        return invalid;
+
+    } else {
+        return 2;
+    }
+}
+
+/* Reads data from the magnetometer and stores it in data
+ * Returns:
+ * 0 if successful.
+ * 1 if the i2c read failed. */
+uint8_t QMCGetMag(qmc_t * sensor, struct magData * data) {
+    uint8_t buffer[6];
+    if(QMCReadBytes(sensor, QMC_XOUT_LSB, 6, buffer) == 6) {
+        data->x = buffer[0] | (buffer[1] << 8);
+        data->y = buffer[2] | (buffer[3] << 8);
+        data->z = buffer[4] | (buffer[4] << 8);
+    } else {
+        return 1;
+    }
+}
---
+++ firmware/src/qmc5883l.h
@@ -0,0 +1,139 @@
+/* A simple driver for the QMC5883L magnetometer chip from QST corporation */
+
+#ifndef QMC5883L_H
+#define QMC5883L_H
+
+#include <pico/stdlib.h>
+#include <hardware/i2c.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+/* This isnt quite exhaustive, but it contains everything we care about */
+enum QMCRegister {
+    QMC_XOUT_LSB = 0x00,
+    QMC_XOUT_MSB = 0x01,
+    QMC_YOUT_LSB = 0x02,
+    QMC_YOUT_MSB = 0x03,
+    QMC_ZOUT_LSB = 0x04,
+    QMC_ZOUT_MSB = 0x05,
+    QMC_STATUS   = 0x06,
+    QMC_TEMP_LSB = 0x07,
+    QMC_TEMP_MSB = 0x08,
+    QMC_CONTROL1 = 0x09,
+    QMC_CONTROL2 = 0x0A,
+    QMC_SETRESET = 0x0B
+};
+
+/* Enums for the values that can be written into CONTROL1 */
+
+enum QMCMode {             // The QMC can be put into standby if needed
+    QMC_STANDBY    = 0x00,
+    QMC_CONTINUOUS = 0x01
+};
+
+enum QMCODR {              // Output Data Rates
+    QMC_ODR_10HZ   = 0x00,
+    QMC_ODR_50HZ   = 0x01,
+    QMC_ODR_100HZ  = 0x02,
+    QMC_ODR_200HZ  = 0x03
+};
+
+enum QMCScale {            // Measurements are done on a scale from 0-x Gauss
+    QMC_SCALE_2G   = 0x00,
+    QMC_SCALE_8G   = 0x01
+};
+
+enum QMCOSR {              // OverSample Ratio; how many samples should be
+    QMC_OSR_512    = 0x00, // averaged for a single data point?
+    QMC_OSR_256    = 0x01,
+    QMC_OSR_128    = 0x02,
+    QMC_OSR_64     = 0x03
+};
+
+/* Shifts for CONTROL1 */
+#define QMC_MODE_SHIFT  0
+#define QMC_ODR_SHIFT   2
+#define QMC_SCALE_SHIFT 4
+#define QMC_OSR_SHIFT   6
+
+/* CONTROL2 just takes boolean values, so just define the shifts */
+#define QMC_SOFT_RST    7 // Restore registers to their default values
+#define QMC_ROL_PNT     6 // Enable pointer rolling between 0x00-0x06
+#define QMC_INT_ENB     0 // Enable the interrupt pin. Not wired in on Bob.
+
+/* Likewise, STATUS only contains boolean values */
+#define QMC_DSKIP       2 // Data skip? Not really sure what this does.
+#define QMC_DOVL        1 // Overflow flag. Is high if any output goes out of range.
+#define QMC_DRDY        0 // Data ready. Is high if new data is ready.
+
+#define QMC_ADDR     0x0D
+#define QMC_TIMEOUT  1000
+
+struct qmc_cfg {          // Used for quick setting/analysis of QMC settings.
+    enum QMCMode mode;
+    enum QMCODR ODR;
+    enum QMCScale scale;
+    enum QMCOSR OSR;
+    bool pointerRoll;
+    bool enableInterrupt;
+    uint8_t control1Raw;  // Only used by QMCGetCfg. Can be ignored otherwise.
+    uint8_t control2Raw;
+};
+
+struct qmc_status {
+    bool dataReady;
+    bool dataOverflow;
+    bool dataSkip;
+};
+
+struct magData {
+    int16_t x;
+    int16_t y;
+    int16_t z;
+};
+
+typedef struct qmc_t {
+    i2c_inst_t * i2c;
+    struct qmc_cfg config;
+} qmc_t;
+
+/* Creates a QMC_T */
+qmc_t QMCInit(i2c_inst_t * i2c);
+
+/* The QMC5883L doesnt have any real self-test capability, but we can
+ * at least make sure it is talking properly and if it is configured correctly.
+ * Returns:
+ * 0 if the QMC is talking and is configured to produce data.
+ * 1 if the QMC is talking, has a valid configuration, but is on standby.
+ * 2 if the QMC has an invalid configuration.
+ * 3 if the QMC is not talking */
+uint8_t QMCTest(qmc_t * sensor);
+
+/* Reads and parses the QMC status register, places the result in status
+ * Returns:
+ * 0 if successful
+ * 1 if the I2C read failed */
+uint8_t QMCGetStatus(qmc_t * sensor, struct qmc_status * status);
+
+/* A quick way to configure the QMC5883L, based on config.
+ * N.B. the control 1 & 2 raw fields in config can be safely ignored
+ * Returns:
+ * 0 if successful
+ * 1 if the I2C failed */
+uint8_t QMCSetCfg(qmc_t * sensor, struct qmc_cfg config);
+
+/* Reads and parses the config registers on the QMC5883L
+ * Stores the result in sensor->config, alongside the raw registers.
+ * Returns:
+ * 0 if successful
+ * 1 if the config is invalid
+ * 2 if the I2C failed */
+uint8_t QMCGetCfg(qmc_t * sensor);
+
+/* Reads data from the magnetometer and stores it in data
+ * Returns:
+ * 0 if successful.
+ * 1 if the i2c read failed. */
+uint8_t QMCGetMag(qmc_t * sensor, struct magData * data);
+
+#endif
