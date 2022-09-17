3d3737f34a3d233a4bb04bdefa78d26b9ea7ba72
Rebecca Wardle <mattydwardle@gmail.com>
2022-09-17 14:31:03 +0100

    Removed pico SDK and recursive deps

--- firmware/genBin.sh
+++ firmware/genBin.sh
@@ -5,7 +5,7 @@
 # Any arguments passed to this script will be passed to CMake.
 # You shouldnt need to run this *that* often, but CMake is weird.
 
-# Where are we?
+# Where is the scritp?
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
@@ -14,16 +14,13 @@ git submodule init
 git submodule update
 
 # Tell CMake where the SDK is
-export PICO_SDK_PATH=$SCRIPT_DIR/lib/pico-sdk
+# export PICO_SDK_PATH=$SCRIPT_DIR/lib/pico-sdk
 
 # Tell CMake what we're building for
-export PICO_BOARD_HEADER_DIRS=$SCRIPT_DIR
+# export PICO_BOARD_HEADER_DIRS=$SCRIPT_DIR
 
-if [ ! -d bin ]
-   then
-       mkdir bin
-fi
+mkdir -p bin
 
 # Make the makefiles
 cd bin
-cmake -DPICO_BOARD=bob "$@" ../src
+cmake -DPICO_BOARD=bob -DPICO_BOARD_HEADER_DIRS=$SCRIPT_DIR -DPICO_SDK_PATH=$SCRIPT_DIR"/lib/pico-sdk" "$@" ../src
