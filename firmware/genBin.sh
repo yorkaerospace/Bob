57fe508a810af3389b86cf810886337a654429c2
ProtonNumber <jwhinton@yenno.com>
2022-09-18 16:27:16 +0100

    Passed arguments to CMake again

--- firmware/genBin.sh
+++ firmware/genBin.sh
@@ -2,7 +2,7 @@
 
 # Generates the bin folder and all the makefiles within it.
 # Will automatically clear the bin folder before starting.
-# Set -D to generate a debug build, which will be copied directly to RAM.
+# Any arguments passed to this script will be passed to CMake
 
 # Where is the script?
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
@@ -12,11 +12,10 @@ cd $SCRIPT_DIR
 git submodule init
 git submodule update
 
-CMAKE_ARGS=$(-DPICO_BOARD=bob -DPICO_BOARD_HEADER_DIRS=$SCRIPT_DIR -DPICO_SDK_PATH="$1")
+CMAKE_ARGS=$(-DPICO_BOARD=bob -DPICO_BOARD_HEADER_DIRS=$SCRIPT_DIR $@)
 
 mkdir -p bin
 
 # Make the makefiles
 cd bin
 cmake $CMAKE_ARGS ..
-
