20dbdc3a76545a376ca4978e35569722f50cf9c8
ProtonNumber <jwhinton@yenno.com>
2022-09-25 15:35:27 +0100

    Moved sensor handling onto core 1

---
+++ firmware/src/core1.h
@@ -0,0 +1,3 @@
+/* A simple loop that runs on core 1. Polls the sensors and passes
+ * pointers to data into the FIFO buffer. */
+void core1Entry(void);
--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
+#include "hardware/irq.h"
+#include "pico/multicore.h"
 
 #include "usbcmd.h"
 #include "sensors.h"
+#include "core1.h"
 
+volatile data_t data;
 
+void fifoIRQ() {
+    data_t * data_p;
+    data_p = multicore_fifo_pop_blocking();
+    data = *data_p;
+    free(data_p);
+}
 
 int main() {
     uint8_t status;
-    data_t data;
 
     stdio_init_all();
     usbCmdInit();
-    configureSensors();
 
-    sleep_ms(10000);
-    status = testSensors();
-    printf("Status: %d\n");
+    multicore_fifo_clear_irq();
+    irq_set_exclusive_handler(SIO_IRQ_PROC0, fifoIRQ);
+
+    irq_set_enabled(SIO_IRQ_PROC0, true);
+
+    multicore_launch_core1(core1Entry);
+
     while (true) {
-        data = pollSensors(status);
         status = data.status;
         printf("Status: %d\n"
                "IMU:\n"
