6b42c9db8e1c4491b0c54998c7dde806937cc191
ProtonNumber <jwhinton@yenno.com>
2022-09-25 22:30:32 +0100

    Added header guards

--- firmware/src/core1.h
+++ firmware/src/core1.h
@@ -1,3 +1,8 @@
+#ifndef CORE1_H
+#define CORE1_H
+
 /* A simple loop that runs on core 1. Polls the sensors and passes
  * pointers to data into the FIFO buffer. */
 void core1Entry(void);
+
+#endif
