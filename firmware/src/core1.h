dfc517209e6435b7d31f5779f8b321a5e24091ad
ProtonNumber <jwhinton@yenno.com>
2022-10-18 15:07:48 +0100

    Moved Core1 into main.c

--- firmware/src/core1.h
+++
@@ -1,8 +0,0 @@
-#ifndef CORE1_H
-#define CORE1_H
-
-/* A simple loop that runs on core 1. Polls the sensors and passes
- * pointers to data into the FIFO buffer. */
-void core1Entry(void);
-
-#endif
--- firmware/src/main.c
+++ firmware/src/main.c
@@ -6,7 +6,24 @@
 
 #include "stateMachine.h"
 #include "sensors.h"
-#include "core1.h"
+
+void core1Entry(void) {
+    data_t * data_p;
+    uint8_t status;
+    absolute_time_t nextPoll;
+
+    configureSensors();
+    status = testSensors();
+
+    while (true) {
+        nextPoll = make_timeout_time_ms(10);
+        data_p = malloc(sizeof(data_t));
+        *data_p = pollSensors(status);
+        sleep_until(nextPoll);
+        multicore_fifo_push_timeout_us(data_p, 500);
+        status = data_p->status;
+    }
+}
 
 int main() {
     uint8_t status;
