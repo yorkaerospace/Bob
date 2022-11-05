f49c1025f48dedca80f318a6066c7e3792536ae0
ProtonNumber <jwhinton@yenno.com>
2022-11-05 10:06:09 +0000

    ReadData now takes arguments and trims off whitespace

--- drivers/readData.sh
+++ drivers/readData.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo "r" -> /dev/ttyACM0;
-stdbuf -o0 cat /dev/ttyACM0 >> log.csv
+# Takes data from the bob and yheets it into a file.
+# Usage: readData <tty> <destination>
 
+echo "r" -> $1;
+stdbuf -o0 cat $1 >> $2
+
+sed -i '/?/d' $2
+sed -i '/^$/d' $2
