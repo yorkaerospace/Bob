2ec3c18dcd261c1ee4fc0ebd2b7d9976ac93e2f7
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2023-03-05 17:47:31 +0000

    refactor: tighten up readData script

--- drivers/readData.sh
+++ drivers/readData.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+set -euo pipefail
 
 # Takes data from the bob and yheets it into a file.
 # Usage: readData <tty> <destination>
 
-echo "r" -> $1;
-stdbuf -o0 cat $1 >> $2
+TTY="$1"
+OUTPUT="$2"
 
-sed -i '/?/d' $2
-sed -i '/^$/d' $2
+echo "r" -> "$TTY"
+stdbuf -o0 cat "$TTY" >> "$OUTPUT"
+
+# Remove all '?' characters
+sed -i '/?/d' "$OUTPUT"
+# Remove empty lines
+sed -i '/^$/d' "$OUTPUT"
